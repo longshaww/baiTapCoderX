22d67186d3fbc7877215e9780c6aee8677f9b63e
unknown <at400123@gmail.com>
2020-10-27 20:00:05 +0700

    first commit

---
+++ baiarray.js
@@ -0,0 +1,16 @@
+/**
+ * Cách sử dụng Array và Object khác nhau như thế nào. Viết câu trả lời ở phía dưới.
+ */
+var myDog = {
+  name:'Stupid Dog',
+  age:2,
+  isAlive:"True",
+};
+var myCat = {
+  name:'Cutie Cat',
+  age:2.5,
+  isAlive:"True",
+};
+
+var myPet=[myDog,myCat]
+console.log(myPet[0],myPet[1])
